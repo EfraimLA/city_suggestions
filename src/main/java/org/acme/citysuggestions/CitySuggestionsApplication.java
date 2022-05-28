d63f67e939286ea166dc8e4575f3134d79267c6a
Efraim López <46459026+EfraimLA@users.noreply.github.com>
2022-05-27 22:48:43 -0500

    spring init

---
+++ src/main/java/org/acme/citysuggestions/CitySuggestionsApplication.java
@@ -0,0 +1,13 @@
+package org.acme.citysuggestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitySuggestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitySuggestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/acme/citysuggestions/CitySuggestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.citysuggestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitySuggestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
